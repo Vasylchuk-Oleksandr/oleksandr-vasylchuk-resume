65b998171dc4730f9458a08efbb2fcee7c9a8104
OleksandrVasylchuk <sanyabatyushka@gmail.com>
2023-05-22 13:25:14 +0300

    add work

--- src/Components/Absolute/Absolute.js
+++ src/Components/Absolute/Absolute.js
@@ -9,12 +9,13 @@ export default function Asside() {
         <Name>Oleksandr Vasylchuk</Name>
         <Job>Full Stack</Job>
         <Target>
-          I am a Full Stack developer looking for a full-time and par-time
-          position in a company that's aimed at making both everyday life and
-          business easier, more productive and user-friendly. I want to be in a
-          good team to improve my skills and use them for interesting tasks I
-          have sound of HTML, CSS, JavaScript, PHP, WordPress and Figma. I am a
-          fast learner, responsible, and ready for challenging tasks.
+          I am a Full Stack developer looking for par-time position in a company
+          that's aimed at making both everyday life and business easier, more
+          productive and user-friendly. I want to be in a good team to improve
+          my skills and use them for interesting tasks I have sound of HTML5,
+          CSS3, SASS, PHP, JS, JQUERY, AJAX, WordPress, ACF, Elementor, and
+          Figma. I am a fast learner, responsible, and ready for challenging
+          tasks.
         </Target>
       </Column>
     </Absolute>
